e8e2e4d5bc17449283c006f52b3c5b58700944fb
Suryaprakash Das <107832463+suryaprakash666@users.noreply.github.com>
2023-03-27 19:52:01 +0530

    Add files via upload

---
+++ 21.py
@@ -0,0 +1,20 @@
+class Students:
+
+    def __init__(self, name, age, grade):
+        self.grade = grade
+        self.name = name
+        self.age = age
+
+    def personality(self):
+        if self.grade > 5:
+            print("Good")
+        else:
+            print("Bad")
+
+
+s1 = Students("surya", 21, 6)
+s1.personality()
+print(s1.grade)
+print(s1.name)
+
+
